807eaff38613985b466bf842eb1b22517ae02f4c
majormech <62382005+majormech@users.noreply.github.com>
2026-01-18 19:18:57 -0600

    Create 004_oos_equipment_emails.sql

---
+++ migrations/004_oos_equipment_emails.sql
@@ -0,0 +1,4 @@
+ALTER TABLE email_config ADD COLUMN oos_scba_emails TEXT;
+ALTER TABLE email_config ADD COLUMN oos_scuba_emails TEXT;
+ALTER TABLE email_config ADD COLUMN oos_saw_emails TEXT;
+ALTER TABLE email_config ADD COLUMN oos_gas_emails TEXT;
